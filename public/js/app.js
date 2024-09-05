f10c75022a5fcfb0fec1891aa2912a7408e9ed72
Osvaldo Juan Olea <osvaldo_olea_85@hotmail.com>
2024-09-05 10:23:19 -0300

    Implementar Service Worker

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,20 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     inicializarModales();
+
+    //Registrar Service Worker
+    if('serviceWorker' in navigator){
+        navigator.serviceWorker.register('/public/service-worker.js')
+        .then((registration) => {
+            console.log("Service Worker Registrado", registration.scope);
+        }).catch((error) => {
+            console.error('Error al registrar el services worker', error);
+        })
+    }
     
     auth.onAuthStateChanged((user) => {
         if(user) {
-            // Usuario ha iniciado sesión
+            // Usuario ha iniciado sesión s
             document.getElementById('loginForm').style.display = 'none';
             document.getElementById('registerForm').style.display = 'none';
             document.getElementById('userInfo').style.display = 'block';
@@ -24,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    const myModal = new bootstrap.Modal(document.getElementById('ejercicioModal'));
+    
     
     document.querySelectorAll('.ejercicio img').forEach(img => {
         img.addEventListener('click', function() {
@@ -33,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
             modalTitle.textContent = `Tabla de Pesos - ${ejercicio}`;
             
             cargarTablaDePesos(ejercicio);
+            const myModal = new bootstrap.Modal(document.getElementById('ejercicioModal'));
             myModal.show();
         });
     });
